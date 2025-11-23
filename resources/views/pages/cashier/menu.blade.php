243998bdeb3a7c45e912aa6db4c2e5dd175fe0bf
Fathur <muhammadfathurrahman013@gmail.com>
2025-11-23 11:37:00 +0700

    role

--- resources/views/pages/cashier/menu.blade.php
+++ resources/views/pages/cashier/menu.blade.php
@@ -1,8 +1,77 @@
 <x-layouts.app :title="__('Menu')">
-   <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
-      <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
-         <x-placeholder-pattern class="absolute inset-0 size-full stroke-gray-900/20 dark:stroke-neutral-100/20" />
-         
+  <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+    <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
+      {{-- Card --}}
+      <div class="bg-white-200 dark:bg-zinc-900 rounded-xl shadow overflow-hidden w-50">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOL6qpG44dBdd4xlCxolTgkbvNFt3py1lvPg&s"
+          alt="Menu Image" class="w-full object-cover">
+        <div class="py-3 px-4">
+          <h2 class="text-lg font-semibold">Americano</h2>
+          <h2 class="text-lg">Rp. 12.000,-</h2>
+          <button class="w-full py-2 mt-3 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600">
+            Add
+          </button>
+        </div>
       </div>
-   </div>
+      {{-- Card --}}
+      <div class="bg-white-200 dark:bg-zinc-900 rounded-xl shadow overflow-hidden w-50">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOL6qpG44dBdd4xlCxolTgkbvNFt3py1lvPg&s"
+          alt="Menu Image" class="w-full object-cover">
+        <div class="py-3 px-4">
+          <h2 class="text-lg font-semibold">Americano</h2>
+          <h2 class="text-lg">Rp. 12.000,-</h2>
+          <button class="w-full py-2 mt-3 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600">
+            Add
+          </button>
+        </div>
+      </div>
+      {{-- Card --}}
+      <div class="bg-white-200 dark:bg-zinc-900 rounded-xl shadow overflow-hidden w-50">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOL6qpG44dBdd4xlCxolTgkbvNFt3py1lvPg&s"
+          alt="Menu Image" class="w-full object-cover">
+        <div class="py-3 px-4">
+          <h2 class="text-lg font-semibold">Americano</h2>
+          <h2 class="text-lg">Rp. 12.000,-</h2>
+          <button class="w-full py-2 mt-3 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600">
+            Add
+          </button>
+        </div>
+      </div>
+      {{-- Card --}}
+      <div class="bg-white-200 dark:bg-zinc-900 rounded-xl shadow overflow-hidden w-50">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOL6qpG44dBdd4xlCxolTgkbvNFt3py1lvPg&s"
+          alt="Menu Image" class="w-full object-cover">
+        <div class="py-3 px-4">
+          <h2 class="text-lg font-semibold">Americano</h2>
+          <h2 class="text-lg">Rp. 12.000,-</h2>
+          <button class="w-full py-2 mt-3 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600">
+            Add
+          </button>
+        </div>
+      </div>
+      {{-- Card --}}
+      <div class="bg-white-200 dark:bg-zinc-900 rounded-xl shadow overflow-hidden w-50">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOL6qpG44dBdd4xlCxolTgkbvNFt3py1lvPg&s"
+          alt="Menu Image" class="w-full object-cover">
+        <div class="py-3 px-4">
+          <h2 class="text-lg font-semibold">Americano</h2>
+          <h2 class="text-lg">Rp. 12.000,-</h2>
+          <button class="w-full py-2 mt-3 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600">
+            Add
+          </button>
+        </div>
+      </div>
+      {{-- Card --}}
+      <div class="bg-white-200 dark:bg-zinc-900 rounded-xl shadow overflow-hidden w-50">
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOL6qpG44dBdd4xlCxolTgkbvNFt3py1lvPg&s"
+          alt="Menu Image" class="w-full object-cover">
+        <div class="py-3 px-4">
+          <h2 class="text-lg font-semibold">Americano</h2>
+          <h2 class="text-lg">Rp. 12.000,-</h2>
+          <button class="w-full py-2 mt-3 bg-yellow-500 text-white rounded-lg hover:bg-yellow-600">
+            Add
+          </button>
+        </div>
+      </div>
+    </div>
 </x-layouts.app>
--- routes/web.php
+++ routes/web.php
@@ -8,36 +8,43 @@ use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 })->name('home');
 
-Route::view('cashier/dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::view('cashier/menu', 'pages/cashier/menu')
-    ->middleware(['auth', 'verified'])
-    ->name('menu');
-
-Route::view('cashier/pesanan', 'pages/cashier/pesanan')
-    ->middleware(['auth', 'verified'])
-    ->name('pesanan');
-
 Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
+  Route::redirect('settings', 'settings/profile');
 
-    Route::get('settings/profile', Profile::class)->name('profile.edit');
-    Route::get('settings/password', Password::class)->name('user-password.edit');
-    Route::get('settings/appearance', Appearance::class)->name('appearance.edit');
+  Route::get('settings/profile', Profile::class)->name('profile.edit');
+  Route::get('settings/password', Password::class)->name('user-password.edit');
+  Route::get('settings/appearance', Appearance::class)->name('appearance.edit');
 
-    Route::get('settings/two-factor', TwoFactor::class)
-        ->middleware(
-            when(
-                Features::canManageTwoFactorAuthentication()
-                    && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
-                ['password.confirm'],
-                [],
-            ),
-        )
-        ->name('two-factor.show');
+  Route::get('settings/two-factor', TwoFactor::class)
+    ->middleware(
+      when(
+        Features::canManageTwoFactorAuthentication()
+        && Features::optionEnabled(Features::twoFactorAuthentication(), 'confirmPassword'),
+        ['password.confirm'],
+        [],
+      ),
+    )
+    ->name('two-factor.show');
 });
+
+Route::middleware(['role:cashier'])
+  ->prefix('cashier')
+  ->group(function () {
+
+    Route::get('/menu', function () {
+      return view('cashier.menu');
+    });
+
+    Route::get('/product', function () {
+      return view('cashier.product');
+    });
+
+    Route::get('/transaction', function () {
+      return view('cashier.transaction');
+    });
+
+  }
+);
