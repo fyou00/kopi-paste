b648b9f058a79333a784eb864182264f2c0cc090
Fathur <muhammadfathurrahman013@gmail.com>
2025-11-23 18:59:03 +0700

    landing page

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers\Auth;
-
+use Illuminate\Http\RedirectResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
@@ -10,29 +10,39 @@ use Illuminate\Support\Facades\Auth;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function store(LoginRequest $request): Response
-    {
-        $request->authenticate();
+  /**
+   * Handle an incoming authentication request.
+   */
+  public function store(Request $request): RedirectResponse
+  {
+    $request->authenticate();
+    $request->session()->regenerate();
 
-        $request->session()->regenerate();
+    $user = auth()->user();
 
-        return response()->noContent();
+    if ($user->role === 'admin') {
+      return redirect()->intended('/admin/dashboard');
     }
 
-    /**
-     * Destroy an authenticated session.
-     */
-    public function destroy(Request $request): Response
-    {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
+    if ($user->role === 'cashier') {
+      return redirect()->intended('/cashier/menu');
     }
+
+    return redirect('/');
+  }
+
+
+  /**
+   * Destroy an authenticated session.
+   */
+  public function destroy(Request $request): Response
+  {
+    Auth::guard('web')->logout();
+
+    $request->session()->invalidate();
+
+    $request->session()->regenerateToken();
+
+    return response()->noContent();
+  }
 }
