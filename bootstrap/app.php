c79d48256ada6d6c634dcab52110c8799f8d8c64
Fathur <muhammadfathurrahman013@gmail.com>
2025-12-13 08:44:33 +0700

    fix login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 use App\Http\Middleware\RoleMiddleware;
+use App\Http\Middleware\CheckRole;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
